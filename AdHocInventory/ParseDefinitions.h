d098fa705a457d0377ccf31526ff95d8e8273997
nielubowicz <nielubowicz@gmail.com>
2014-08-26 10:58:10 -0400

    Added Parse framework. Added constant definitions for Parse (key names, class names, etc). Renamed DatabaseManager to InventoryItemManager (no longer manages a database, but the connection to Parse)

---
+++ AdHocInventory/ParseDefinitions.h
@@ -0,0 +1,13 @@
+//
+//  ParseDefinitions.h
+//  AdHocInventory
+//
+//  Created by chris nielubowicz on 8/22/14.
+//  Copyright (c) 2014 Assorted Intelligence. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParseDefinitions : NSObject
+
+@end
---
+++ AdHocInventory/ParseDefinitions.m
@@ -0,0 +1,13 @@
+//
+//  ParseDefinitions.m
+//  AdHocInventory
+//
+//  Created by chris nielubowicz on 8/22/14.
+//  Copyright (c) 2014 Assorted Intelligence. All rights reserved.
+//
+
+#import "ParseDefinitions.h"
+
+@implementation ParseDefinitions
+
+@end
