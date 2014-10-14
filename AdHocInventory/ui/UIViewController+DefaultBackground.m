28a6f343abae0a84610baac2c15f8581520c1cff
nielubowicz <nielubowicz@gmail.com>
2014-10-13 22:51:11 -0400

    Changed colors

--- AdHocInventory/ui/UIViewController+DefaultBackground.m
+++ AdHocInventory/ui/UIViewController+DefaultBackground.m
@@ -10,18 +10,29 @@
 
 @implementation UIViewController (DefaultBackground)
 
+static const UIColor *lightColor = nil;
+static const UIColor *darkColor = nil;
+
 - (void) setDefaultBackground
 {
+    if (lightColor == nil) {
+        lightColor = [UIColor colorWithRed:255/255.0 green:237/255.0 blue:229/255.0 alpha:1.0];
+    }
+    
+    if (darkColor == nil) {
+        darkColor = [UIColor colorWithRed:242/255.0 green:226/255.0 blue:210/255.0 alpha:1.0];
+    }
+
     CGFloat width = 8.0f;
     CGRect rect = CGRectMake(0.0f, 0.0f, width, 1.0f);
     UIGraphicsBeginImageContext(rect.size);
     CGContextRef context = UIGraphicsGetCurrentContext();
     
-    CGContextSetFillColorWithColor(context, [[UIColor colorWithWhite:0.75 alpha:1.0] CGColor]);
+    CGContextSetFillColorWithColor(context, [lightColor CGColor]);
     CGRect leftRect = CGRectMake(0.0f, 0.0f, width/2.0f, 1.0f);
     CGContextFillRect(context, leftRect);
     
-    CGContextSetFillColorWithColor(context, [[UIColor colorWithWhite:0.85 alpha:1.0] CGColor]);
+    CGContextSetFillColorWithColor(context, [darkColor CGColor]);
     CGRect rightRect = CGRectMake(width/2.0f, 0.0f, width/2.0f, 1.0f);
     CGContextFillRect(context, rightRect);
     
